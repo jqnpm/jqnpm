ab7f3a9b6e0aa74e2d001c3657d4481bb6d285e9
Joel Purra <code+github@joelpurra.com>
2014-12-31 10:43:53 +0100

    Run all tests in subdirectories

---
+++ tests/all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+while IFS= read -r -d '' file;
+do
+	dirpath=$(dirname "$file")	
+	dirname=$(basename -a "$dirpath")
+	filename=$(basename -a "$file")
+	echo "${dirname}/${filename}"
+
+	# Execute tests in a subshell in their own directory.
+	pushd "$dirpath" >/dev/null
+	( "$file" )
+	popd >/dev/null
+done < <(find "$PWD" -mindepth 2 -maxdepth 2 -type f -name '*.sh' -print0)
