f492cb86b7ac63107b9ffc279be9cd530f678ef9
Joel Purra <code+github@joelpurra.com>
2014-12-31 18:02:40 +0100

    To create a new package/project, run jqnpm init

---
+++ tests/init-in-empty-directory/test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+source "${BASH_SOURCE%/*}/../prepare.sh"
+
+
+function cleanup {
+	git clean -fdX . >/dev/null
+}
+
+function oneTimeSetUp () {
+	cleanup
+}
+
+function tearDown () {
+	cleanup
+}
+
+function testInit () {
+	assertFalse "jq.json doesn't exist" "[[ -s 'local-project/jq.json' ]]"
+
+	pushd "local-project" >/dev/null
+	jqnpm init
+	popd >/dev/null
+
+	assertTrue "src/main.jq is generated" "[[ -s 'local-project/src/main.jq' ]]"
+	assertTrue "jq.json is generated" "[[ -s 'local-project/jq.json' ]]"
+	assertTrue "jq.json empty dependencies" "[[ $(<"local-project/jq.json" jq '.dependencies | length') == '0' ]]"
+	assertTrue "jq.json has package name same as this folder" "[[ $(<"local-project/jq.json" jq '.name') == '"local-project"' ]]"
+	assertTrue "jq.json has package version" "[[ $(<"local-project/jq.json" jq '.version') == '"0.1.0"' ]]"
+	assertTrue "jq.json has jq version" "[[ $(<"local-project/jq.json" jq '.engines["jq"]') == '"^1.5.0"' ]]"
+	assertTrue "jq.json has jqnpm version" "[[ $(<"local-project/jq.json" jq '.engines["jqnpm"]') == '"^1.0.0"' ]]"
+}
+
+
+source "${BASH_SOURCE%/*}/../test-runner.sh"
