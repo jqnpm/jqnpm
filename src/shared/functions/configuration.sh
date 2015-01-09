c4fff070d147df9e0d23fe67ca9dc6096cbec686
Joel Purra <code+github@joelpurra.com>
2015-01-09 07:44:13 +0100

    Move configuration values to a separate file

---
+++ src/shared/functions/configuration.sh
@@ -0,0 +1,4 @@
+# TODO: normalize configuration, configuration defaults and internal variable names.
+readonly config_default_packagesCache="$HOME/.jq/packages"
+readonly config_default_remoteBase="git://github.com"
+readonly config_default_remoteSuffix=".git"
